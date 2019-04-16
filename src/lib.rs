50233a32d86bd1fe7ed51a37d2e5b08c13e0e95b
Talha Zengin <zengintalha@gmail.com>
2019-04-16 20:56:39 +0300

    windows regex fix.

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub fn get() -> Option<IpAddr> {
     let stdout = String::from_utf8(output.stdout).unwrap();
 
     let re = Regex::new(r#"(?m)^.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
+
     for cap in re.captures_iter(&stdout) {
         if let Some(host) = cap.at(2) {
             if host != "127.0.0.1" {
@@ -38,6 +39,7 @@ pub fn get() -> Option<IpAddr> {
     let stdout = String::from_utf8(output.stdout).unwrap();
 
     let re = Regex::new(r#"(?m)^.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
+
     for cap in re.captures_iter(&stdout) {
         if let Some(host) = cap.at(2) {
             if host != "127.0.0.1" {
@@ -53,6 +55,27 @@ pub fn get() -> Option<IpAddr> {
 
     None
 }
+
+// LINUX EXAMPLE IFCONFIG
+// eth0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+//         inet 172.17.0.2  netmask 255.255.0.0  broadcast 0.0.0.0
+//         inet6 fe80::42:acff:fe11:2  prefixlen 64  scopeid 0x20<link>
+//         ether 02:42:ac:11:00:02  txqueuelen 0  (Ethernet)
+//         RX packets 20775  bytes 151512533 (144.4 MiB)
+//         RX errors 0  dropped 0  overruns 0  frame 0
+//         TX packets 21587  bytes 2799736 (2.6 MiB)
+//         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+// lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
+//         inet 127.0.0.1  netmask 255.0.0.0
+//         inet6 ::1  prefixlen 128  scopeid 0x10<host>
+//         loop  txqueuelen 1000  (Local Loopback)
+//         RX packets 11001  bytes 570940 (557.5 KiB)
+//         RX errors 0  dropped 0  overruns 0  frame 0
+//         TX packets 11001  bytes 570940 (557.5 KiB)
+//         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+
 
 #[cfg(target_os = "windows")]
 pub fn get() -> Option<IpAddr> {
@@ -62,7 +85,8 @@ pub fn get() -> Option<IpAddr> {
 
     let stdout = String::from_utf8(output.stdout).unwrap();
     
-    let re = Regex::new(r#"(?m)^.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
+    let re = Regex::new(r#"(?m)^.*Address. . . . . . . . . . . : (Addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
+
     for cap in re.captures_iter(&stdout) {
         if let Some(host) = cap.at(2) {
             if host != "127.0.0.1" {
@@ -78,3 +102,34 @@ pub fn get() -> Option<IpAddr> {
 
     None
 }
+
+// WINDOWS EXAMPLE IPCONFIG
+// Windows IP Configuration
+
+// Ethernet adapter Ethernet:
+
+//    Media State . . . . . . . . . . . : Media disconnected
+//    Connection-specific DNS Suffix  . : home
+
+// Wireless LAN adapter Local Area Connection* 2:
+
+//    Media State . . . . . . . . . . . : Media disconnected
+//    Connection-specific DNS Suffix  . :
+
+// Wireless LAN adapter Local Area Connection* 3:
+
+//    Media State . . . . . . . . . . . : Media disconnected
+//    Connection-specific DNS Suffix  . :
+
+// Ethernet adapter Ethernet 2:
+
+//    Media State . . . . . . . . . . . : Media disconnected
+//    Connection-specific DNS Suffix  . :
+
+// Wireless LAN adapter Wi-Fi:
+
+//    Connection-specific DNS Suffix  . : home
+//    Link-local IPv6 Address . . . . . : fe80::9091:50fe:cf97:9af6%11
+//    IPv4 Address. . . . . . . . . . . : 192.168.1.47
+//    Subnet Mask . . . . . . . . . . . : 255.255.255.0
+//    Default Gateway . . . . . . . . . : 192.168.1.1
