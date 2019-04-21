1894301ed9f6cba2337973bce1e9e14a9877f052
Talha Zengin <zengintalha@gmail.com>
2019-04-21 23:13:15 +0300

    refactor..

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use std::process::Command;
 use std::net::{IpAddr, Ipv4Addr};
+
 use regex::Regex;
 
 // LINUX EXAMPLE IFCONFIG
 //
 // eth0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
-//         inet 172.17.0.2  netmask 255.255.0.0  broadcast 0.0.0.0
+//         inet 172.17.0.2  netmask 255.255.0.0  broadcast 0.0.0.0gf
 //         inet6 fe80::42:acff:fe11:2  prefixlen 64  scopeid 0x20<link>
 //         ether 02:42:ac:11:00:02  txqueuelen 0  (Ethernet)
 //         RX packets 20775  bytes 151512533 (144.4 MiB)
@@ -31,9 +32,9 @@ pub fn get_local_ip() -> Option<IpAddr> {
 
     let stdout = String::from_utf8(output.stdout).unwrap();
 
-    let re = Regex::new(r#"(?m)^.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
+    let regex = Regex::new(r#"(?m)^.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
 
-    for cap in re.captures_iter(&stdout) {
+    for cap in regex.captures_iter(&stdout) {
         if let Some(host) = cap.at(2) {
             if host != "127.0.0.1" {
                 if let Ok(addr) = host.parse::<Ipv4Addr>() {
@@ -54,9 +55,9 @@ pub fn get_local_ip() -> Option<IpAddr> {
 
     let stdout = String::from_utf8(output.stdout).unwrap();
 
-    let re = Regex::new(r#"(?m)^.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
+    let regex = Regex::new(r#"(?m)^.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
 
-    for cap in re.captures_iter(&stdout) {
+    for cap in regex.captures_iter(&stdout) {
         if let Some(host) = cap.at(2) {
             if host != "127.0.0.1" {
                 if let Ok(addr) = host.parse::<Ipv4Addr>() {
@@ -104,9 +105,9 @@ pub fn get_local_ip() -> Option<IpAddr> {
 
     let stdout = String::from_utf8(output.stdout).unwrap();
     
-    let re = Regex::new(r#"(?m)^.*IPv4 Address. . . . . . . . . . . : (Addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
+    let regex = Regex::new(r#"(?m)^.*IPv4 Address. . . . . . . . . . . : (Addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
 
-    for cap in re.captures_iter(&stdout) {
+    for cap in regex.captures_iter(&stdout) {
         if let Some(host) = cap.at(2) {
             if let Ok(addr) = host.parse::<Ipv4Addr>() {
                 return Some(IpAddr::V4(addr))
